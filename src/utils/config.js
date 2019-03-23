c0adb53ba52c402d82df9acb72ce1f190b349850
ZhaiXiaoyuan <2549004118@qq.com>
2019-03-23 09:10:11 +0800

    20190323

--- src/utils/config.js
+++ src/utils/config.js
@@ -7,7 +7,7 @@ export default {
            /* domain:isDev?'http://api.linqi.tech':'',*/
             domain:isDev?'http://api.linqi.tech':'http://api.linqi.tech',
            /* domain2:isDev?'http://api.linqi.tech:8600':'',*/
-            domain2:isDev?'/api':'http://api.linqi.tech:8600',
+            domain2:false&&isDev?'/api':'http://api.linqi.tech:8600',
             fileDomain:isDev?'http://api.linqi.tech':'http://api.linqi.tech',
         }
     },
